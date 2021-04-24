de0a34e12127d77087b97b400596f372bd6e98c9
Eugen Wiens <wiens.eugens@gmail.com>
2021-04-25 01:22:45 +0200

    added Task test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,65 @@
 #include <Arduino.h>
+#include <TaskScheduler.h>
 
+#include "Logger.hpp"
+
+
+class SchedulerContainer {
+public:
+  static Scheduler* getScheduler()
+  {
+    static Scheduler scheduler;
+
+    return &scheduler;
+  }
+};
+
+class Task1 : public Task
+{
+public:
+  Task1()
+  : Task(1000, TASK_FOREVER, SchedulerContainer::getScheduler(), true)
+  {
+    debugLog();
+  }
+
+  virtual bool Callback() override
+  {
+    debugLog() << "test";
+
+    return true;
+  }
+};
+
+class Task2 : public Task
+{
+public:
+  Task2()
+  : Task(2000, TASK_FOREVER, SchedulerContainer::getScheduler(), true)
+  {
+    debugLog();
+
+  }
+
+protected:
+  virtual bool Callback() override
+  {
+    debugLog() << "test2";
+    return true;
+  }
+};
+
+Scheduler *pScheduler = SchedulerContainer::getScheduler();
+
+Task1 t1;
+Task2 t2;
 
 void setup() {
   Serial.begin(115200);
 
+  Serial.println("setup");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(2000);
+  pScheduler->execute();
 }
