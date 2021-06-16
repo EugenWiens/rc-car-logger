1f2dd393dff2b5cb077d4f307f6e4416d87e1733
Eugen Wiens <wiens.eugens@gmail.com>
2021-06-16 23:02:43 +0200

    first UI integrated
    * added first menu elements in a loop
    * added icon provider to abstract the icons

---
+++ libs/ui/src/SettingsMenuDialog.cpp
@@ -0,0 +1,13 @@
+
+#include "SettingsMenuDialog.hpp"
+
+void SettingsMenuDialog::draw(int x, int y)
+{
+
+}
+
+bool SettingsMenuDialog::handleEvent(const EventManager::UiEvent &event)
+{
+    return false;
+}
+
